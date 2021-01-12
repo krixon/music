38f4b78b65309d1783251b470524432637f8a6ca
Karl Rixon <karlrixon@gmail.com>
2021-01-12 16:41:24 +0000

    Remove unused Pitch operators and methods

--- Krixon.Music.Core/Pitch.cs
+++ Krixon.Music.Core/Pitch.cs
@@ -15,49 +15,6 @@ namespace Krixon.Music.Core
             return new Pitch(Parser.NoteLetter(str), Parser.Accidentals(str));
         }
 
-        public static implicit operator int(Pitch pitch)
-        {
-            return (pitch.NoteLetter switch
-            {
-                NoteLetter.C => 0,
-                NoteLetter.D => 2,
-                NoteLetter.E => 4,
-                NoteLetter.F => 5,
-                NoteLetter.G => 7,
-                NoteLetter.A => 9,
-                NoteLetter.B => 11,
-                _ => throw new ArgumentOutOfRangeException()
-            } + pitch.Accidentals) % 12;
-        }
-
-        public static implicit operator Pitch(int pitch)
-        {
-            pitch %= 12;
-            if (pitch < 0) pitch += 12;
-
-            return pitch switch
-            {
-                0 => new Pitch(NoteLetter.C, 0),
-                1 => new Pitch(NoteLetter.C, 1),
-                2 => new Pitch(NoteLetter.D, 0),
-                3 => new Pitch(NoteLetter.D, 1),
-                4 => new Pitch(NoteLetter.E, 0),
-                5 => new Pitch(NoteLetter.F, 0),
-                6 => new Pitch(NoteLetter.F, 1),
-                7 => new Pitch(NoteLetter.G, 0),
-                8 => new Pitch(NoteLetter.G, 1),
-                9 => new Pitch(NoteLetter.A, 0),
-                10 => new Pitch(NoteLetter.A, 1),
-                11 => new Pitch(NoteLetter.B, 0),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-        }
-
-        public Pitch Transpose(int semitones)
-        {
-            return this + semitones;
-        }
-
         private static class Parser
         {
             public static NoteLetter NoteLetter(string str)
