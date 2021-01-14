621dc044039eac9b47430b82bf00e1bf3c3985ff
Karl Rixon <karlrixon@gmail.com>
2021-01-14 17:24:25 +0000

    Implement harmonic minor scales

--- Krixon.Music.Core.Test/Scales/ScaleTests.cs
+++ Krixon.Music.Core.Test/Scales/ScaleTests.cs
@@ -197,6 +197,33 @@ namespace Krixon.Music.Core.Test.Scales
             TestNotes(tonic, expected, Scale.Locrian);
         }
 
+        [Test]
+        [TestCase("Cb", new[] {"Cb", "Db", "Ebb", "Fb", "Gb", "Abb", "Bb"})]
+        [TestCase("C", new[] {"C", "D", "Eb", "F", "G", "Ab", "B"})]
+        [TestCase("C#", new[] {"C#", "D#", "E", "F#", "G#", "A", "B#"})]
+        [TestCase("Db", new[] {"Db", "Eb", "Fb", "Gb", "Ab", "Bbb", "C5"})]
+        [TestCase("D", new[] {"D", "E", "F", "G", "A", "Bb", "C#5"})]
+        [TestCase("D#", new[] {"D#", "E#", "F#", "G#", "A#", "B", "C##5"})]
+        [TestCase("Eb", new[] {"Eb", "F", "Gb", "Ab", "Bb", "Cb5", "D5"})]
+        [TestCase("E", new[] {"E", "F#", "G", "A", "B", "C5", "D#5"})]
+        [TestCase("E#", new[] {"E#", "F##", "G#", "A#", "B#", "C#5", "D##5"})]
+        [TestCase("Fb", new[] {"Fb", "Gb", "Abb", "Bbb", "Cb5", "Dbb5", "Eb5"})]
+        [TestCase("F", new[] {"F", "G", "Ab", "Bb", "C5", "Db5", "E5"})]
+        [TestCase("F#", new[] {"F#", "G#", "A", "B", "C#5", "D5", "E#5"})]
+        [TestCase("Gb", new[] {"Gb", "Ab", "Bbb", "Cb5", "Db5", "Ebb5", "F5"})]
+        [TestCase("G", new[] {"G", "A", "Bb", "C5", "D5", "Eb5", "F#5"})]
+        [TestCase("G#", new[] {"G#", "A#", "B", "C#5", "D#5", "E5", "F##5"})]
+        [TestCase("Ab", new[] {"Ab", "Bb", "Cb5", "Db5", "Eb5", "Fb5", "G5"})]
+        [TestCase("A", new[] {"A", "B", "C5", "D5", "E5", "F5", "G#5"})]
+        [TestCase("A#", new[] {"A#", "B#", "C#5", "D#5", "E#5", "F#5", "G##5"})]
+        [TestCase("Bb", new[] {"Bb", "C5", "Db5", "Eb5", "F5", "Gb5", "A5"})]
+        [TestCase("B", new[] {"B", "C#5", "D5", "E5", "F#5", "G5", "A#5"})]
+        [TestCase("B#", new[] {"B#", "C##5", "D#5", "E#5", "F##5", "G#5", "A##5"})]
+        public static void HarmonicMinor(string tonic, string[] expected)
+        {
+            TestNotes(tonic, expected, Scale.HarmonicMinor);
+        }
+
         private static void TestNotes(string tonic, string[] expected, Func<Note, Scale> ctor)
         {
             var scale = ctor(Note.Parse(tonic));
--- Krixon.Music.Core/Scales/Scale.cs
+++ Krixon.Music.Core/Scales/Scale.cs
@@ -4,7 +4,7 @@ using Krixon.Music.Core.Intervals;
 
 namespace Krixon.Music.Core.Scales
 {
-    public struct Scale
+    public readonly struct Scale
     {
         public IEnumerable<Note> Notes { get; }
         public Note Tonic { get; }
@@ -20,48 +20,56 @@ namespace Krixon.Music.Core.Scales
         }
 
         public static Scale Major(Note tonic) => Ionian(tonic);
-        public static Scale Ionian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(1));
-        public static Scale Dorian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(2));
-        public static Scale Phrygian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(3));
-        public static Scale Lydian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(4));
-        public static Scale Mixolydian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(5));
-        public static Scale Aeolian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(6));
-        public static Scale Locrian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(7));
+        public static Scale Minor(Note tonic) => Aeolian(tonic);
+        public static Scale HarmonicMinor(Note tonic) => new(tonic, Diatonic(6, Augment(7)));
+        public static Scale Ionian(Note tonic) => new(tonic, Diatonic(1));
+        public static Scale Dorian(Note tonic) => new(tonic, Diatonic(2));
+        public static Scale Phrygian(Note tonic) => new(tonic, Diatonic(3));
+        public static Scale Lydian(Note tonic) => new(tonic, Diatonic(4));
+        public static Scale Mixolydian(Note tonic) => new(tonic, Diatonic(5));
+        public static Scale Aeolian(Note tonic) => new(tonic, Diatonic(6));
+        public static Scale Locrian(Note tonic) => new(tonic, Diatonic(7));
 
         private static IEnumerable<Note> CreateNotesFromIntervals(Note tonic, IEnumerable<Interval> intervals)
         {
             return intervals.Select(tonic.Transpose).Prepend(tonic);
         }
 
-        private static class IntervalsFactory
+        public static Interval[] Diatonic(int mode, IDictionary<int, int>? adjustments = null)
         {
-            public static Interval[] Diatonic(int mode)
+            // T-T-s-T-T-T-s
+            var sequence = RotateSequence(new[] {2, 2, 1, 2, 2, 2, 1}, mode);
+            var intervals = new Interval[6];
+            var semitones = 0;
+
+            for (var i = 0; i < 6; i++)
             {
-                // T-T-s-T-T-T-s
-                var sequence = RotateSequence(new[] {2, 2, 1, 2, 2, 2, 1}, mode);
-                var intervals = new Interval[6];
-                var semitones = 0;
+                // If step n is to be augmented or diminished, the interval n-1 must be adjusted accordingly.
+                var adjustment = adjustments?.ContainsKey(i + 2) ?? false ? adjustments[i + 2] : 0;
 
-                for (var i = 0; i < 6; i++)
-                {
-                    semitones += sequence[i];
-                    intervals[i] = Interval.FromSemitoneCount(semitones, (Number) i + 1);
-                }
-
-                return intervals;
+                semitones += sequence[i];
+                intervals[i] = Interval.FromSemitoneCount(semitones + adjustment, (Number) i + 1);
             }
 
-            private static int[] RotateSequence(int[] sequence, int mode)
+            return intervals;
+        }
+
+        /// <summary>
+        /// Creates a adjustments dictionary which augments specific steps of the scale by a semitone.
+        /// </summary>
+        /// <param name="steps">The 1-based step in the scale to augment.</param>
+        public static Dictionary<int, int> Augment(params int[] steps) => steps.ToDictionary(s => s, _ => 1);
+
+        private static int[] RotateSequence(int[] sequence, int mode)
+        {
+            var result = new int[sequence.Length];
+
+            for (var i = 0; i < sequence.Length; i++)
             {
-                var result = new int[sequence.Length];
-
-                for (var i = 0; i < sequence.Length; i++)
-                {
-                    result[i] = sequence[(i + mode - 1) % sequence.Length];
-                }
-
-                return result;
+                result[i] = sequence[(i + mode - 1) % sequence.Length];
             }
+
+            return result;
         }
     }
 }
