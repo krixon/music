5c5e4354d15b593e4990c907204b452883d6bbb9
Karl Rixon <karlrixon@gmail.com>
2021-01-14 14:23:03 +0000

    Allow number to be hinted when creating an interval from semitones
    This allows the correct interval to be determined since multiple
    intervals share the same number of semitones. In the context of a scale,
    this avoids either duplicating a note letter or having to adjust
    specific intervals after creation.

--- Krixon.Music.Core/Intervals/Interval.cs
+++ Krixon.Music.Core/Intervals/Interval.cs
@@ -16,21 +16,49 @@ namespace Krixon.Music.Core.Intervals
         }
 
         public static Interval Unison() => new(0, Quality.Perfect, Number.Unison);
+        public static Interval AugmentedUnison() => new(1, Quality.Augmented, Number.Unison);
+
+        public static Interval DiminishedSecond() => new(0, Quality.Diminished, Number.Second);
         public static Interval MinorSecond() => new(1, Quality.Minor, Number.Second);
         public static Interval MajorSecond() => new(2, Quality.Major, Number.Second);
+        public static Interval AugmentedSecond() => new(3, Quality.Augmented, Number.Second);
+
+        public static Interval DiminishedThird() => new(2, Quality.Diminished, Number.Third);
         public static Interval MinorThird() => new(3, Quality.Minor, Number.Third);
         public static Interval MajorThird() => new(4, Quality.Major, Number.Third);
+        public static Interval AugmentedThird() => new(5, Quality.Augmented, Number.Third);
+
+        public static Interval DiminishedFourth() => new(4, Quality.Diminished, Number.Fourth);
         public static Interval PerfectFourth() => new(5, Quality.Perfect, Number.Fourth);
         public static Interval AugmentedFourth() => new(6, Quality.Augmented, Number.Fourth);
+
         public static Interval DiminishedFifth() => new(6, Quality.Diminished, Number.Fifth);
         public static Interval PerfectFifth() => new(7, Quality.Perfect, Number.Fifth);
+        public static Interval AugmentedFifth() => new(8, Quality.Augmented, Number.Fifth);
+
+        public static Interval DiminishedSixth() => new(7, Quality.Diminished, Number.Sixth);
         public static Interval MinorSixth() => new(8, Quality.Minor, Number.Sixth);
         public static Interval MajorSixth() => new(9, Quality.Major, Number.Sixth);
+        public static Interval AugmentedSixth() => new(10, Quality.Augmented, Number.Sixth);
+
+        public static Interval DiminishedSeventh() => new(9, Quality.Diminished, Number.Seventh);
         public static Interval MinorSeventh() => new(10, Quality.Minor, Number.Seventh);
         public static Interval MajorSeventh() => new(11, Quality.Major, Number.Seventh);
+        public static Interval AugmentedSeventh() => new(12, Quality.Augmented, Number.Seventh);
+
+        public static Interval DiminishedOctave() => new (11, Quality.Diminished, Number.Octave);
         public static Interval Octave() => new (12, Quality.Perfect, Number.Octave);
 
-        public static Interval FromSemitoneCount(int semitoneCount)
+        /// <summary>
+        /// Creates a new interval based on a number of semitones.
+        /// </summary>
+        /// <param name="semitoneCount"></param>
+        /// <param name="number">
+        /// A hint as to which interval number is sought. This can be used to resolve ambiguities
+        /// since multiple intervals share a semitone count. If a number is specified which does not make
+        /// sense due to the semitone count, it is ignored.
+        /// </param>
+        public static Interval FromSemitoneCount(int semitoneCount, Number? number = null)
         {
             var position = semitoneCount % 12;
 
@@ -39,18 +67,31 @@ namespace Krixon.Music.Core.Intervals
 
             return position switch
             {
+                0 when number == Number.Second => DiminishedSecond(),
                 0 => Unison(),
+                1 when number == Number.Unison => AugmentedUnison(),
                 1 => MinorSecond(),
+                2 when number == Number.Third => DiminishedThird(),
                 2 => MajorSecond(),
+                3 when number == Number.Second => AugmentedSecond(),
                 3 => MinorThird(),
+                4 when number == Number.Fourth => DiminishedFourth(),
                 4 => MajorThird(),
+                5 when number == Number.Third => AugmentedThird(),
                 5 => PerfectFourth(),
+                6 when number == Number.Fourth => AugmentedFourth(),
                 6 => DiminishedFifth(),
+                7 when number == Number.Sixth => DiminishedSixth(),
                 7 => PerfectFifth(),
+                8 when number == Number.Fifth => AugmentedFifth(),
                 8 => MinorSixth(),
+                9 when number == Number.Seventh => DiminishedSeventh(),
                 9 => MajorSixth(),
+                10 when number == Number.Sixth => AugmentedSixth(),
                 10 => MinorSeventh(),
+                11 when number == Number.Octave => DiminishedOctave(),
                 11 => MajorSeventh(),
+                12 when number == Number.Seventh => AugmentedSeventh(),
                 12 => Octave(),
                 _ => throw new ArgumentOutOfRangeException(nameof(semitoneCount), semitoneCount, null)
             };
--- Krixon.Music.Core/Scales/Scale.cs
+++ Krixon.Music.Core/Scales/Scale.cs
@@ -23,25 +23,14 @@ namespace Krixon.Music.Core.Scales
         public static Scale Ionian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(1));
         public static Scale Dorian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(2));
         public static Scale Phrygian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(3));
-        public static Scale Lydian(Note tonic)
-        {
-            var intervals = IntervalsFactory.Diatonic(4);
-
-            // TODO: Handle this within the interval factory by considering the current step of the scale as
-            //       well as the number of semitones. In this case, the fourth is augmented (6 semitones), but with
-            //       just the semitones we would end up with a diminished fifth.
-            intervals[2] = Interval.AugmentedFourth();
-
-            return new(tonic, intervals);
-        }
-
+        public static Scale Lydian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(4));
         public static Scale Mixolydian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(5));
         public static Scale Aeolian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(6));
         public static Scale Locrian(Note tonic) => new(tonic, IntervalsFactory.Diatonic(7));
 
         private static IEnumerable<Note> CreateNotesFromIntervals(Note tonic, IEnumerable<Interval> intervals)
         {
-            return intervals.SkipLast(1).Select(tonic.Transpose).Prepend(tonic);
+            return intervals.Select(tonic.Transpose).Prepend(tonic);
         }
 
         private static class IntervalsFactory
@@ -50,13 +39,13 @@ namespace Krixon.Music.Core.Scales
             {
                 // T-T-s-T-T-T-s
                 var sequence = RotateSequence(new[] {2, 2, 1, 2, 2, 2, 1}, mode);
-                var intervals = new Interval[7];
+                var intervals = new Interval[6];
                 var semitones = 0;
 
-                for (var i = 0; i < sequence.Length; i++)
+                for (var i = 0; i < 6; i++)
                 {
                     semitones += sequence[i];
-                    intervals[i] = Interval.FromSemitoneCount(semitones);
+                    intervals[i] = Interval.FromSemitoneCount(semitones, (Number) i + 1);
                 }
 
                 return intervals;
